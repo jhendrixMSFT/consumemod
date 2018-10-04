252d7f0d5ba65d92722aff0037563daae102183d
Joel Hendrix <jhendrix@microsoft.com>
2018-10-04 14:33:36 -0700

    update to fake sdk

--- go.mod
+++ go.mod
@@ -1,4 +1,3 @@
 module github.com/jhendrixMSFT/consumemod
 
-//github.com/jhendrixMSFT/moremods/bar v1.0.0
-require github.com/jhendrixMSFT/moremods/profiles/latest v1.0.0
+require github.com/jhendrixMSFT/go-fake-sdk v1.0.0
--- main.go
+++ main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 
-	//"github.com/jhendrixMSFT/moremods/bar/2018-01-01/bar"
-	//"github.com/jhendrixMSFT/modtest/foo"
-	//oldfoo "github.com/jhendrixMSFT/modtest/foo/2018-01-01/foo"
-	//newfoo "github.com/jhendrixMSFT/modtest/foo/2018-02-01/foo"
-	"github.com/jhendrixMSFT/moremods/profiles/latest/bar"
-	"github.com/jhendrixMSFT/moremods/profiles/latest/foo"
+	"github.com/jhendrixMSFT/go-fake-sdk/services/bar/2018-01-01/bar"
+	oldfoo "github.com/jhendrixMSFT/go-fake-sdk/services/foo/2018-01-01/foo"
+	newfoo "github.com/jhendrixMSFT/go-fake-sdk/services/foo/2018-02-01/foo"
 )
 
 func main() {
-	//fmt.Println(bar.Report())
-	//fmt.Println(oldfoo.Report())
-	//fmt.Println(newfoo.Report())
 	fmt.Println(bar.Report())
-	fmt.Println(foo.Report())
+	fmt.Println(oldfoo.Report())
+	fmt.Println(newfoo.Report())
 }
