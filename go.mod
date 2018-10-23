636e80b526faa47bf6388e2294cf91f3cc84816e
Joel Hendrix <jhendrix@microsoft.com>
2018-10-23 10:52:57 -0700

    update to latest mock SDK

--- go.mod
+++ go.mod
@@ -1,11 +1,6 @@
 module consumemod
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dimchansky/utfbom v1.0.0 // indirect
-	github.com/jhendrixMSFT/azure-sdk-for-go2/profiles/2017-03-09 v0.1.0
-	github.com/jhendrixMSFT/go-autorest2 v0.1.0
-	github.com/mitchellh/go-homedir v1.0.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute v1.0.0
+	github.com/jhendrixMSFT/go-autorest2/v11 v11.3.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/jhendrixMSFT/azure-sdk-for-go2/profiles/2017-03-09/resources/mgmt/resources"
-	"github.com/jhendrixMSFT/go-autorest2/autorest/azure/auth"
+	"github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute"
+	"github.com/jhendrixMSFT/go-autorest2/v11/autorest/azure/auth"
 )
 
 func main() {
@@ -13,14 +10,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	client := resources.NewClient("subID")
+	client := compute.NewVirtualMachinesClient("subID")
 	client.Authorizer = a
-	for page, err := client.List(context.Background(), "", "", nil); page.NotDone(); err = page.Next() {
-		if err != nil {
-			panic(err)
-		}
-		for _, v := range page.Values() {
-			fmt.Println(*v.ID)
-		}
-	}
 }
