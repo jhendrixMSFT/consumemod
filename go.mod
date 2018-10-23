630f44c502b7fd9eced7deb7d6c9fe0d5b5d78a9
Joel Hendrix <jhendrix@microsoft.com>
2018-10-23 12:49:53 -0700

    use v2 module

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module consumemod
+module github.com/jhendrixMSFT/consumemod
 
 require (
-	github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute v1.0.0
+	github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute/v2 v2.0.0
 	github.com/jhendrixMSFT/go-autorest2/v11 v11.3.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute"
+	"github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute/v2"
 	"github.com/jhendrixMSFT/go-autorest2/v11/autorest/azure/auth"
 )
 
@@ -12,4 +12,5 @@ func main() {
 	}
 	client := compute.NewVirtualMachinesClient("subID")
 	client.Authorizer = a
+	client.FakeV2Method()
 }
