4edf7b0a81002c161f079449873ce81acedec08e
Joel Hendrix <jhendrix@microsoft.com>
2018-10-24 14:08:29 -0700

    update to latest mock SDK

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute/v2"
-	"github.com/jhendrixMSFT/go-autorest2/v11/autorest/azure/auth"
+	"github.com/jhendrixMSFT/azure-sdk-for-go2/services/compute/mgmt/2016-03-30/compute"
+	"github.com/jhendrixMSFT/go-autorest2/autorest/azure/auth"
 )
 
 func main() {
@@ -12,5 +12,4 @@ func main() {
 	}
 	client := compute.NewVirtualMachinesClient("subID")
 	client.Authorizer = a
-	client.FakeV2Method()
 }
